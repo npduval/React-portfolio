38c2976d141f98836ac92e50f6038709505f63d4
Nick Duval <nicholaspduval74@gmail.com>
2022-02-24 21:49:32 -0500

    using gh-page

---
+++ build/precache-manifest.d6be8f5c6834a1f93ce8e6bb503fdf6f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b124f9f3b8bc915251a06b23aaed03b",
+    "url": "/React-portfolio/index.html"
+  },
+  {
+    "revision": "ee010581f6e0ce4d8d24",
+    "url": "/React-portfolio/static/css/main.08325502.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/React-portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee010581f6e0ce4d8d24",
+    "url": "/React-portfolio/static/js/main.81b10ead.chunk.js"
+  },
+  {
+    "revision": "10ccf993e81dbe9c6081",
+    "url": "/React-portfolio/static/js/runtime-main.247b40e1.js"
+  },
+  {
+    "revision": "17703bd999e15c6752f9d305b1cbc70d",
+    "url": "/React-portfolio/static/media/dog-adoption.17703bd9.png"
+  },
+  {
+    "revision": "07770787f6241705279dd6ad40ea0bb1",
+    "url": "/React-portfolio/static/media/istockphoto.07770787.jpg"
+  },
+  {
+    "revision": "fc673cc73b416809619ca10e8b854c37",
+    "url": "/React-portfolio/static/media/password-generator.fc673cc7.png"
+  },
+  {
+    "revision": "dfbe4289e71d273386c1229f0bc55c6d",
+    "url": "/React-portfolio/static/media/readme-generator.dfbe4289.png"
+  },
+  {
+    "revision": "1874462272335820b4eb213d4e2193c3",
+    "url": "/React-portfolio/static/media/social-network.18744622.png"
+  },
+  {
+    "revision": "84a47dca38f5841ebf4233034f821634",
+    "url": "/React-portfolio/static/media/text-editor.84a47dca.png"
+  },
+  {
+    "revision": "ab761c3140e4264cd66010a73173c932",
+    "url": "/React-portfolio/static/media/weather-dashboard.ab761c31.png"
+  }
+]);
